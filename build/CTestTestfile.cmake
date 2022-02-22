c9b3f2d4e27c6fe55ff81b4fd8bd0489b4fe1006
minhongpark <minhong10003@gmail.com>
2022-02-22 11:07:11 -0800

    testing

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/park/rc_car/src
+# Build directory: /home/park/rc_car/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/park/rc_car/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/park/rc_car/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/park/rc_car/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/park/rc_car/src:$ROS_PACKAGE_PATH"
